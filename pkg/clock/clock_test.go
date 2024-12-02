ebb5fa38eedd846d6d71b73c2583cd5e738e8e93
Nicolas GORAJ <github@goraj.fr>
2024-12-02 21:41:47 +0100

    test(clock): add missing test

--- pkg/clock/clock_test.go
+++ pkg/clock/clock_test.go
@@ -1,7 +1,24 @@
 package clock
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestClock(t *testing.T) {
+
+	t.Run("The clock return the current time", func(t *testing.T) {
+		realClock := NewRealClock()
+
+		actual := realClock.Now().UnixNano()
+		expected := time.Now().UnixNano()
+
+		diff := expected - actual
+
+		if diff > 1000 {
+			t.Errorf("Difference between timestamp is too big: %v", diff)
+		}
+
+	})
 
 }
